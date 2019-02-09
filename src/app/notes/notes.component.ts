d1f89fdcfbc25c44f655c50ed4d65228436f422c
Piyush Kumar Gupta <Piyush.9.Gupta@in.niit.com>
2019-02-09 17:44:36 +0530

    iSpring conversions added by piyush

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -16,14 +16,18 @@ export class NotesComponent implements OnInit {
   @ViewChild("videoWrapper") videoWrapper: ElementRef;
   @ViewChild("paginate") paginate: any;
 
-  private data: any;
+  public data: any;
   private assetsPath: string = "../../assets/";
   private ppt: string = "";
   private handouts: string = "";
   private keynotes:string="";
+  private isHandouts:any =null;
+  public notHandouts:any =null;
   closePreTab: boolean = true;
   closeHangoutsTab: boolean = true;
   showWrapper: boolean = false;
+  private changeStatus:boolean = false;
+  public p:any=null;
   
   framePath: any = this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/images/image78.png");
   constructor(private http: HttpClient, private domSanitizer: DomSanitizer, private renderer: Renderer2) { }
@@ -38,15 +42,30 @@ export class NotesComponent implements OnInit {
 
   }
 
-  changeFrame(index) {
-	  console.log(this.paginate);
-	  // var abc = this.paginate.nativeElement.childNodes[0].childNodes[0].childNodes[1].childNodes[4].childNodes[1].children[1].innerHTML;
-	  // if(index!=abc){
-		  // index = index + 20;
-	  // }
+  changeFrame(index,page_num=null) {
+ 
+	this.isHandouts = '';
+	this.notHandouts = '';
+	if(page_num == 2){
+		index = index + 20;
+	}
     var frame = this.data[index];
+	if(frame.handouts_options){
+		this.isHandouts=frame.handouts_options;
+	}
+	if(frame.handouts)
+	{
+		this.notHandouts=frame.handouts;
+		
+		
+	}else if(!frame.handouts_options){
+this.closeHangoutsTab=true;	 
+		this.notHandouts= "false";
+	}
+	
     var video = this.assetsPath + frame["video"];
     this.ppt = frame["ppt"];
+	
     this.handouts = frame["handouts"];
     this.keynotes=frame["keynotes"];
    
@@ -70,15 +89,18 @@ export class NotesComponent implements OnInit {
     const container = doc.getElementById("animation_container");
     const canvas = doc.getElementById("canvas");
     const dom_overlay_container = doc.getElementById("dom_overlay_container");
+	if(container && container.style){
+		 container.style.width = "350px";
+		container.style.height = "215px";
 
-    container.style.width = "350px";
-    container.style.height = "215px";
-
-    canvas.style.width = "350px";
-    canvas.style.height = "212px";
-
-    dom_overlay_container.style.width = "350px";
-    dom_overlay_container.style.height = "212px";
+		canvas.style.width = "350px";
+		canvas.style.height = "212px";
+	}
+   
+	if(dom_overlay_container && dom_overlay_container.style){
+		dom_overlay_container.style.width = "350px";
+		dom_overlay_container.style.height = "212px";
+	}
 
   }
   downloadPresentaion() {
@@ -104,10 +126,22 @@ export class NotesComponent implements OnInit {
     if (type && this.handouts) {
       location.href = (<string>this.assetsPath + "handouts/level_2/" + this.handouts + type + ".doc");
     }
+	
+	else if(type && this.isHandouts){
+		
+		location.href = (<string>this.assetsPath + "handouts/level_2/" + this.isHandouts + ".doc");
+	}
     else {
       return false;
     }
   }
+  // private changeStatus(){
+	  // if(!this.changeStatus){
+		  // this.changeStatus = true;
+	  // }else{
+		  // this.changeStatus = false;
+	  // }
+  // }
 
 
 }
