9cc446a0ba2891d692fa0a0c9cd2552c1635aaac
Piyush Gupta <piyushgupta0307@gmail.com>
2019-02-15 16:58:42 +0530

    Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import {NgxPaginationModule} from 'ngx-pagination'; 
@@ -10,19 +10,24 @@ import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { ResizerDirective } from './directives/resizer.directive';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbdTooltipBasic } from './tooltip-basic';
+
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
     ResizerDirective,
+    // NgbdTooltipBasics
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgxPaginationModule,
-    FormsModule    
-
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
