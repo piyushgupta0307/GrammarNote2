5917ec08e00eb4e62ca49ee88f7c21265233cc6b
Piyush Gupta <piyushgupta0307@gmail.com>
2019-03-01 18:01:33 +0530

    By Piyush

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -19,56 +19,68 @@ export class NotesComponent implements OnInit {
   public data: any;
   private assetsPath: string = "../../assets/";
   private ppt: string = "";
-  private handouts: string = "";
-  private keynotes:string="";
-  private isHandouts:any =null;
-  public notHandouts:any =null;
+  public handouts: any = "";
+  private keynotes: string = "";
+  private isHandouts: any = null;
+  public notHandouts: any = null;
   closePreTab: boolean = true;
   closeHangoutsTab: boolean = true;
   showWrapper: boolean = false;
-  private changeStatus:boolean = false;
-  public p:any=null;
+  private changeStatus: boolean = false;
+  public p: any = null;
+  public downloadPresenation: any = "presentations_for_windows";
+  public downloadhandouts: any = "_oyo";
 
   framePath: any = this.domSanitizer.bypassSecurityTrustResourceUrl("../../assets/images/image78.png");
   constructor(private http: HttpClient, private domSanitizer: DomSanitizer, private renderer: Renderer2) { }
 
   ngOnInit() {
-
     this.http.get(this.assetsPath + "data/data.json").subscribe((data) => {
       this.data = data;
+      if (data) {
+        this.ppt = this.data[0].ppt;
+        this.keynotes = this.data[0].keynotes;
+        this.handouts = this.data[0].handouts;
+      }
     }, (error) => {
       console.log(error);
     });
 
   }
 
-  changeFrame(index,page_num=null) {
- 
-	this.isHandouts = '';
-	this.notHandouts = '';
-	if(page_num == 2){
-		index = index + 20;
-	}
+  changeFrame(index, page_num = null, dPPT, dHout) {
+    this.isHandouts = '';
+    this.notHandouts = '';
+    if (dPPT != 'presentations_for_windows') {
+      this.downloadPresenation = 'presentations_for_windows';
+    }
+    if (dHout != '_oyo') {
+      this.downloadhandouts = '_oyo';
+    }
+
+    if (page_num == 2) {
+      index = index + 20;
+    }
     var frame = this.data[index];
-	if(frame.handouts_options){
-		this.isHandouts=frame.handouts_options;
-	}
-	if(frame.handouts)
-	{
-		this.notHandouts=frame.handouts;
-		
-		
-	}else if(!frame.handouts_options){
-this.closeHangoutsTab=true;	 
-		this.notHandouts= "false";
-	}
-	
+    if (frame.handouts_options) {
+      this.isHandouts = frame.handouts_options;
+      this.downloadhandouts = '_prac';
+    }
+    if (frame.handouts) {
+      this.notHandouts = frame.handouts;
+
+
+    } else if (!frame.handouts_options) {
+      this.closeHangoutsTab = true;
+      this.notHandouts = "false";
+    }
+
     var video = this.assetsPath + frame["video"];
     this.ppt = frame["ppt"];
-	
+
     this.handouts = frame["handouts"];
-    this.keynotes=frame["keynotes"];
-   
+    this.keynotes = frame["keynotes"];
+
     this.framePath = this.domSanitizer.bypassSecurityTrustResourceUrl(video);
     document.getElementById("iframe").onload = this.align;
     this.createResier();
@@ -89,30 +101,29 @@ this.closeHangoutsTab=true;
     const container = doc.getElementById("animation_container");
     const canvas = doc.getElementById("canvas");
     const dom_overlay_container = doc.getElementById("dom_overlay_container");
-	if(container && container.style){
-		 container.style.width = "350px";
-		container.style.height = "215px";
+    if (container && container.style) {
+      container.style.width = "350px";
+      container.style.height = "215px";
 
-		canvas.style.width = "350px";
-		canvas.style.height = "212px";
-	}
-   
-	if(dom_overlay_container && dom_overlay_container.style){
-		dom_overlay_container.style.width = "350px";
-		dom_overlay_container.style.height = "212px";
-	}
+      canvas.style.width = "350px";
+      canvas.style.height = "212px";
+    }
+
+    if (dom_overlay_container && dom_overlay_container.style) {
+      dom_overlay_container.style.width = "350px";
+      dom_overlay_container.style.height = "212px";
+    }
 
   }
   downloadPresentaion() {
-    
+
     const type = this.presentaionFrom.value["downloadPresenation"];
-    if (type !=="keynotes" && this.ppt) {
-  location.href = (<string>this.assetsPath + "ppt/" + type + "/level_2/" + this.ppt);
+    if (type !== "keynotes" && this.ppt) {
+      location.href = (<string>this.assetsPath + "ppt/" + type + "/level_2/" + this.ppt);
     }
-   
-    if(type==="keynotes" && this.keynotes)
-    {
-      location.href = (<string>this.assetsPath + "keynotes/level_2/" + this.keynotes+".zip");
+
+    if (type === "keynotes" && this.keynotes) {
+      location.href = (<string>this.assetsPath + "keynotes/level_2/" + this.keynotes + ".zip");
 
     }
     else {
@@ -121,15 +132,15 @@ this.closeHangoutsTab=true;
   }
   downloadHandouts() {
 
-    const type = this.handoutsFrom.value["handouts"];
+    const type = this.handoutsFrom.value["downloadhandouts"];
     if (type && this.handouts) {
       location.href = (<string>this.assetsPath + "handouts/level_2/" + this.handouts + type + ".doc");
     }
-	
-	else if(type && this.isHandouts){
-		
-		location.href = (<string>this.assetsPath + "handouts/level_2/" + this.isHandouts + ".doc");
-	}
+
+    else if (type && this.isHandouts) {
+
+      location.href = (<string>this.assetsPath + "handouts/level_2/" + this.isHandouts + ".doc");
+    }
     else {
       return false;
     }
